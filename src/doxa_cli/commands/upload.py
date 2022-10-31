c87edf18bc89c1a623184de5760917fba9b1fc42
Siqi Zhu <foursevenn.zsq@gmail.com>
2022-10-31 13:30:36 +0000

    upload without debugging

--- src/doxa_cli/commands/upload.py
+++ src/doxa_cli/commands/upload.py
@@ -1,5 +1,7 @@
 import click
-
+import requests
+from doxa_cli.constants import COMPETITION_KEY, ENVIRONMENT_KEY, UPLAOD_URL, SUCCESS_KEY, ERROR_KEY
+from doxa_cli.utils import get_access_token, read_doxa_upload, compress_agent_dir
 
 @click.command()
 @click.argument("directory", nargs=1, type=str)
@@ -15,11 +17,30 @@ def upload(directory, competition, environment):
     # language: python
     # entrypoint: evaluate.py
 
+    try:
+        upload_json = read_doxa_upload(directory)
+    except FileNotFoundError:
+        click.secho(
+            "\nYou must have doxa.yaml file before upload.", fg="cyan", bold=True
+        )
+        return
+
     # we can override competition and environment with command-line options,
     # which we should validate if used
 
     # if the user does not specify an environment, maybe could let it be the
     # default environment (?)
+    
+    competition = check_variable(competition, COMPETITION_KEY, upload_json)
+    environment = check_variable(environment, ENVIRONMENT_KEY, upload_json)
+    upload_json.pop("competition", None)
+    upload_json.pop("environment", None)
+
+    if environment is None or competition is None:
+        click.secho(
+            "\nYou must specify a environment value either in doxa.yaml or command line flag.", fg="cyan", bold=True
+        )
+        return
 
     # Step 2: POST /api/extern/upload-slot
 
@@ -38,13 +59,77 @@ def upload(directory, competition, environment):
     # Response (failure):
     # { "success": false, "error": { "message": "error msg" } }
 
+    try:
+        upload_response = get_upload_slot(competition, environment, upload_json)
+    except:
+        click.secho("\nAn error occurred while getting a upload slot. Please try again later.", fg="red", bold=True)
+        return
+    
+    success = upload_response[SUCCESS_KEY]
+    if not success:
+        try:
+            assert "error" in upload_json
+        except:
+            click.secho("\nIt seems like something is wrong with the api protocol. 'error' is not in the response.", fg="red", bold=True)
+            return
+
+        click.secho(f"\n{upload_json[ERROR_KEY]}", fg="red", bold=True)
+        click.secho("Please change the variable and try again!", fg="red", bold=True)
+        return
+    else:
+        try:
+            assert "endpoint" in upload_json
+            assert "token" in upload_json
+        except:
+            click.secho("\nIt seems like something is wrong with the api protocol. 'endpoint' or 'token' is not in the response.", fg="red", bold=True)
+        
+        end_point = upload_response["endpoint"]
+        token = upload_response["token"]
+
     # Step 3: produce tar.gz of {directory} using `tarfile` module
+    compress_agent_dir(directory)
 
     # Step 4:`upload the tarfile to {endpoint} making sure to pass in the header
     # - Authorization: Bearer {token from step 2}
+    upload_result = upload_agent()
+    print(upload_result)
 
     # show a fancy progress bar of the upload!
 
     # Step 5: print success message! (or error message)
 
-    click.secho("TODO", bold=True, fg="green")
+def check_variable(v, v_key, json):
+    if v is not None:
+        pass
+    elif v_key in json:
+        v = json[v_key]
+    return v
+
+def get_upload_slot(competition, environment, metadata):
+    session_stop, access_token = get_access_token()
+    if session_stop:
+        return
+
+    headers = {"Authorization": f"Bearer {access_token}"}
+    result = requests.post(
+        UPLAOD_URL, headers=headers,json={"competition_tag": competition, "environment_tag": environment, "metadata": metadata}, verify=True
+    ).json()
+
+    assert SUCCESS_KEY in result
+
+    return result
+
+def upload_agent():
+    session_stop, access_token = get_access_token()
+    if session_stop:
+        return
+
+    headers = {"Authorization": f"Bearer {access_token}"}
+    result = requests.post(
+        UPLAOD_URL, headers=headers,files={"file": open("tarfile.tar.gz", "rb")}, verify=True
+    ).json()
+
+    #assert SUCCESS_KEY in result
+
+    return result
+
--- src/doxa_cli/commands/user.py
+++ src/doxa_cli/commands/user.py
@@ -3,13 +3,8 @@ import json
 
 import click
 import requests
-from doxa_cli.constants import CONFIG_PATH, USER_URL
-from doxa_cli.utils import (
-    clear_doxa_config,
-    is_refresh_required,
-    read_doxa_config,
-    refresh_oauth_token,
-)
+from doxa_cli.constants import USER_URL
+from doxa_cli.utils import get_access_token
 
 
 def print_line(key: str, value: str):
@@ -21,48 +16,9 @@ def print_line(key: str, value: str):
 def user(extra):
     """Display DOXA account information. You must be logged in."""
 
-    try:
-        config = read_doxa_config()
-    except FileNotFoundError:
-        click.secho(
-            "\nYou must be logged in to show user information.", fg="cyan", bold=True
-        )
+    session_stop, access_token = get_access_token()
+    if session_stop:
         return
-    except (ValueError, AssertionError):
-        # oops, the config is corrupted!
-        click.secho(
-            "\nOops, the DOXA CLI configuration file could not be read properly.\n",
-            fg="yellow",
-            bold=True,
-        )
-
-        try:
-            clear_doxa_config()
-            click.secho("Please log in again to fix this issue.", fg="green", bold=True)
-        except:
-            click.secho(
-                "The DOXA CLI was unable to reset its configuration.\n",
-                fg="red",
-                bold=True,
-            )
-            click.secho(
-                f"Please manually delete the file at the following path: {CONFIG_PATH}."
-            )
-
-        return
-
-    if is_refresh_required(config["expires_at"]):
-        try:
-            access_token = refresh_oauth_token()
-        except:
-            click.secho(
-                "\nYour session has expired. Please log in again.",
-                fg="yellow",
-                bold=True,
-            )
-            return
-    else:
-        access_token = config["access_token"]
 
     headers = {"Authorization": f"Bearer {access_token}"}
 
@@ -116,3 +72,5 @@ def user(extra):
     click.secho(
         f"\nYou created your account {diff.days} days ago.", fg="green", bold=True
     )
+
+
