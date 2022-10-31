2d141c84085289c010bf795577f747094fcb5719
Siqi Zhu <foursevenn.zsq@gmail.com>
2022-10-31 13:31:17 +0000

    upload without debugging

--- src/doxa_cli/constants.py
+++ src/doxa_cli/constants.py
@@ -10,6 +10,7 @@ DOXA_BASE_URL = os.environ.get("DOXA_BASE_URL", "https://doxaai.com")
 LOGIN_URL = f"{DOXA_BASE_URL}/api/oauth/device/code"
 TOKEN_URL = f"{DOXA_BASE_URL}/api/oauth/token"
 USER_URL = f"{DOXA_BASE_URL}/api/oauth/user"
+UPLAOD_URL = f"{DOXA_BASE_URL}/api/extern/upload-slot"
 
 CLIENT_ID = "eb594ca3-023d-477f-823a-22e48f4e5235"
 SCOPE = "basic"
@@ -28,6 +29,12 @@ def get_config_directory():
 
 CONFIG_DIRECTORY = get_config_directory()
 CONFIG_PATH = os.path.join(CONFIG_DIRECTORY, "config.json")
+
+UPLOAD_FILE = "doxa.yaml"#os.path.join(CURRENT_DIRECTORY, "doxa.yaml")
+COMPETITION_KEY = "competition"
+ENVIRONMENT_KEY = "environment"
+SUCCESS_KEY = "success"
+ERROR_KEY = "error"
 
 SPINNER = {
     "interval": 80,
--- src/doxa_cli/utils.py
+++ src/doxa_cli/utils.py
@@ -3,8 +3,11 @@ import json
 import os
 
 import requests
+import click
+import yaml
+import tarfile
 
-from doxa_cli.constants import CONFIG_DIRECTORY, CONFIG_PATH, TOKEN_URL
+from doxa_cli.constants import CONFIG_DIRECTORY, CONFIG_PATH, TOKEN_URL, UPLOAD_FILE, UPLOAD_FILE
 
 
 def read_doxa_config() -> dict:
@@ -58,3 +61,69 @@ def refresh_oauth_token(data):
             default=str,
         )
     return token["access_token"]
+
+def get_access_token():
+    session_stop = True
+    access_token = None
+
+    try:
+        config = read_doxa_config()
+    except FileNotFoundError:
+        click.secho(
+            "\nYou must be logged in to show user information.", fg="cyan", bold=True
+        )
+        return (session_stop, access_token)
+
+    except (ValueError, AssertionError):
+        # oops, the config is corrupted!
+        click.secho(
+            "\nOops, the DOXA CLI configuration file could not be read properly.\n",
+            fg="yellow",
+            bold=True,
+        )
+
+        try:
+            clear_doxa_config()
+            click.secho("Please log in again to fix this issue.", fg="green", bold=True)
+        except:
+            click.secho(
+                "The DOXA CLI was unable to reset its configuration.\n",
+                fg="red",
+                bold=True,
+            )
+            click.secho(
+                f"Please manually delete the file at the following path: {CONFIG_PATH}."
+            )
+
+            return (session_stop, access_token)
+
+    if is_refresh_required(config["expires_at"]):
+        try:
+            access_token = refresh_oauth_token()
+        except:
+            click.secho(
+                "\nYour session has expired. Please log in again.",
+                fg="yellow",
+                bold=True,
+            )
+            return (session_stop, access_token)
+    else:
+        access_token = config["access_token"]
+        session_stop = False
+        return (session_stop, access_token)
+
+def read_doxa_upload(directory) -> dict:
+    path = os.path.join(directory, UPLOAD_FILE)
+    with open(path, 'r') as file:
+        config = yaml.safe_load (file)
+
+        #assert "competition" in config
+        #assert "environment" in config
+
+        return config
+
+def compress_agent_dir(directory) -> None:
+    print("compressing the agent directory")
+    #path = os.path.join("upload.tar", UPLOAD_FILE)
+    file = tarfile.open("directory", "w")
+    file.close()
